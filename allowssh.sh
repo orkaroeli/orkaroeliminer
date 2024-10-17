7e2d4c08f8fd39198f11d09fdad8039a7b4bfe5f
orkaroeli <86050638+orkaroeli@users.noreply.github.com>
2024-10-17 03:43:49 -0300

    Create allowssh.sh

---
+++ allowssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install openssh-server
+sudo systemctl status ssh
+sudo systemctl start ssh
+sudo apt install ufw
+sudo systemctl enable ssh
+sudo ufw allow ssh
+sudo ufw allow 22
+sudo ufw reload
+
+hostname -I
